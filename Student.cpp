1e74ea245e5ba4ae979b73b6eefdce694da7f972
DeagleM <W0258876@campus.nscc.ca>
2024-11-23 22:55:04 -0400

    student set up as struct

--- Student.cpp
+++ Student.cpp
@@ -3,3 +3,10 @@
 //
 
 #include "Student.h"
+
+using namespace std;
+
+Student::Student(const string &firstName, const string &lastName, const string &studentNumber,
+                 double midterm1, double midterm2, double finalExam)
+    : firstName(firstName), lastName(lastName), studentNumber(studentNumber), midterm1(midterm1), midterm2(midterm2), finalExam(finalExam) {}
+
--- Student.h
+++ Student.h
@@ -5,10 +5,18 @@
 #ifndef STUDENT_H
 #define STUDENT_H
 
+#include <string>
 
+using namespace std;
 
-class Student {
+struct Student {
+    string firstName;
+    string lastName;
+    string studentNumber;
+    double midterm1, midterm2, finalExam;
 
+    Student(const string &firstName, const string &lastName, const string &studentNumber,
+            double midterm1, double midterm2, double finalExam);
 };
 
 
